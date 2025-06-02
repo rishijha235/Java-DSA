38ac22b306e459e2cf9ddbe8daebab70b3eb8561
rishijha235 <rishijha568@gmail.com>
2025-06-02 19:31:17 +0530

    changed arguments

--- Recursion/CheckSorted.java
+++ Recursion/CheckSorted.java
@@ -1,25 +1,25 @@
 public class CheckSorted {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 6, 5};
+        int[] arr = {1, 2, 3, 4, 7, 8, 5};
         boolean result = isSorted(arr);
         System.out.println(result);
     }
 
     // Check if an array is sorted in ascending order using recursion
     static boolean isSorted(int[] arr) {
-        return helper(arr, 0, 1);
+        return helper(arr, 0);
     }
 
-    private static boolean helper(int[] arr, int left, int right) {
-        if(arr[left] > arr[right]) {
-            return false;
-        }
-
-        if(right == arr.length - 1) {
+    private static boolean helper(int[] arr, int index) {
+        if(index == arr.length - 1) {
             return true;
         }
 
-        return helper(arr, left + 1, right + 1);
+        if(arr[index] > arr[index + 1]){
+            return false;
+        }
+
+        return   helper(arr, index + 1);
     }
 
 }
