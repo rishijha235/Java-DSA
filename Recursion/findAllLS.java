115c482dc345b9c9df7076c7ace31b48c04d29ac
rishijha235 <rishijha568@gmail.com>
2025-06-03 01:30:01 +0530

    new function added with arraylist returntype

--- Recursion/findAllLS.java
+++ Recursion/findAllLS.java
@@ -5,23 +5,33 @@ public class findAllLS {
         int[] arr = {9, 18, 13, 9, -9, 0, 11, 9};
         int target = 9;
 
-        findAllIndex(arr, target, 0);
-        if(list.isEmpty()) {
-            System.out.println("Element not found in the array.");
-        } else {
-            System.out.println("Element found at indices: " + list);
-        } 
+        // findAllIndex(arr, target, 0);
+        // System.out.println(list);
+
+        System.out.println(findAllIndex(arr, target, 0, new ArrayList<>()));
     }
 
     // Linear search using recursion to find all target exists in the array
-    static ArrayList<Integer> list = new ArrayList<>();
-    public static void findAllIndex(int[] arr, int target, int index) {
+    // static ArrayList<Integer> list = new ArrayList<>();
+    // public static void findAllIndex(int[] arr, int target, int index) {
+    //     if(index == arr.length) {
+    //         return; // target not found
+    //     }
+    //     if(arr[index] == target){
+    //         list.add(index);
+    //     }
+    //     findAllIndex(arr, target, index + 1);
+    // }
+
+    //return arraylist
+    public static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
         if(index == arr.length) {
-            return; // target not found
+            return list; // target not found
         }
         if(arr[index] == target){
             list.add(index);
         }
-        findAllIndex(arr, target, index + 1);
+        return findAllIndex(arr, target, index + 1, list);
     }
 }
+
