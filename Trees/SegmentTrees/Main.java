ffdf22cc7b885059092665cc24b4fec126f3a4f3
Rishi Jha <rishijha568@gmail.com>
2025-07-01 23:17:41 +0530

    main file updated

--- Trees/SegmentTrees/Main.java
+++ Trees/SegmentTrees/Main.java
@@ -5,6 +5,7 @@ class Main {
     int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
     SegmentTree tree = new SegmentTree(arr);
     // tree.display();
+    // tree.update(2, 7);
 
     System.out.println(tree.query(1, 6));
   }
