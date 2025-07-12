081a4a4067e0a5ac23c6cd91137b4c5e68f61d9c
Rishi Jha <rishijha568@gmail.com>
2025-07-12 23:03:21 +0530

    leetcode actual solution added

--- Trees/Problems/SD.java
+++ Trees/Problems/SD.java
@@ -45,3 +45,45 @@ public class SD {
         return node;
     }
 }
+
+// // Leetcode solutoin 
+// public class Codec {
+
+//     // Encodes a tree to a single string.
+//     public String serialize(TreeNode root) {
+//         StringBuilder sb = new StringBuilder();
+
+//         serializeHelper(root, sb);
+
+//         return sb.toString();
+//     }
+
+//     private void serializeHelper(TreeNode root, StringBuilder sb) {
+//         if(root == null) {
+//             sb.append("null,");
+//             return;
+//         }
+
+//         sb.append(root.val).append(",");
+//         serializeHelper(root.left, sb);
+//         serializeHelper(root.right, sb);
+//     }
+
+//     // Decodes your encoded data to tree.
+//     public TreeNode deserialize(String data) {
+//     Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(",")));
+//     return deserializeHelper(nodes);
+//     }
+
+//     private TreeNode deserializeHelper(Queue<String> nodes) {
+//         String val = nodes.poll();
+//         if (val.equals("null")) {
+//             return null;
+//         }
+
+//         TreeNode node = new TreeNode(Integer.parseInt(val));
+//         node.left = deserializeHelper(nodes);
+//         node.right = deserializeHelper(nodes);
+//         return node;
+//     }
+// }
