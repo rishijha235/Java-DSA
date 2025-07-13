a84c278bc6295cbe55aa2d3729c348d146c17f6c
Rishi Jha <rishijha568@gmail.com>
2025-07-13 23:26:14 +0530

    new file added

---
+++ Trees/Problems/ValidPath.java
@@ -0,0 +1,25 @@
+public class ValidPath {
+    
+    public static boolean isValidPath(Node node, int[] arr) {
+        if(node == null) {
+            return arr.length == 0;
+        }
+        return helper(node, arr, 0);
+    }
+
+    private static boolean helper(Node node, int[] arr, int index) {
+        if(node == null) {
+            return false;
+        }
+
+        if(index >= arr.length || node.val != arr[index]) {
+            return false;
+        }
+
+        if(node.left == null && node.right == null && index == arr.length - 1) {
+            return true;
+        }
+
+        return helper (node.left, arr, index + 1) || helper(node.right, arr, index + 1);
+    }
+}
